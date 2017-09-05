d16d2e18b81a89629385af8513dd1a729f645315
Larry Chen <lchen@suse.com>
2017-09-05 14:51:57 +0800

    install .tmux.conf to HOME

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git_dir=$(dirname $SCRIPT)
 theme=cyan
 theme_file=${git_dir}/tmux-themepack/powerline/block/${theme}.tmuxtheme
 
-usr_tmux_conf=./.tmux.conf
+usr_tmux_conf=~/.tmux.conf
 cat ${git_dir}/tmux.conf >> ${usr_tmux_conf}
 echo "source-file ${theme_file}" >> ${usr_tmux_conf}
 
