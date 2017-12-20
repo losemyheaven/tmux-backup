a20db2b4a1ae44bf0bd48966edc5b1c260e85f15
Larry Chen <lchen@suse.com>
2017-12-20 19:46:05 +0800

    install with update submodule

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ theme=cyan
 theme_file=${git_dir}/tmux-themepack/powerline/block/${theme}.tmuxtheme
 
 usr_tmux_conf=~/.tmux.conf
+
+git submodule init
+git submodule update
 cat ${git_dir}/tmux.conf > ${usr_tmux_conf}
 echo "source-file ${theme_file}" >> ${usr_tmux_conf}
 
